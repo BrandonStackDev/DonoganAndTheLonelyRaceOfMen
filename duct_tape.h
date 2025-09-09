640af5e45270f0e5f66739165a2e883316ea5346
brandon stack <BrandonStack156@gmail.com>
2025-09-09 01:40:19 -0400

    truck building taped

--- duct_tape.h
+++ duct_tape.h
@@ -23,14 +23,19 @@ typedef struct
 
 
 
-#define gEnvBoundingBoxCount 2
+#define gEnvBoundingBoxCount 7
 
 EnvBoundingBox gEnvBoundingBoxes[gEnvBoundingBoxCount] = { 0 };
 
 void GoGoGadgetDuctTape()
 {
-    gEnvBoundingBoxes[0] = (EnvBoundingBox){EBBT_WALL,(BoundingBox) {(Vector3) {2960.0f, 320.0f, 4026.0f},(Vector3) {2987.0f, 330.0f, 4027.0f}}};
-    gEnvBoundingBoxes[1] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 2960.0f, 320.0f, 4058.0f },(Vector3) { 2987.0f, 330.0f, 4059.0f } } };
+    gEnvBoundingBoxes[0] = (EnvBoundingBox){EBBT_WALL,(BoundingBox) {(Vector3) {2960.0f, 320.0f, 4026.0f},(Vector3) {2987.0f, 340.0f, 4027.0f}}};//don home side wall one
+    gEnvBoundingBoxes[1] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 2960.0f, 320.0f, 4058.0f },(Vector3) { 2987.0f, 340.0f, 4059.0f } } };//son home side wall
+    gEnvBoundingBoxes[2] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 1281.0f, 327.0f, 1270.0f },(Vector3) { 1288.0f, 340.0f, 1272.0f } } };//truck short
+    gEnvBoundingBoxes[3] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 1259.0f, 327.0f, 1270.0f },(Vector3) { 1266.0f, 340.0f, 1272.0f } } };//truck short
+    gEnvBoundingBoxes[4] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 1257.8f, 327.0f, 1243.0f },(Vector3) { 1261.0f, 340.0f, 1270.0f } } };//truck side
+    gEnvBoundingBoxes[5] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 1286.0f, 327.0f, 1243.0f },(Vector3) { 1289.2f, 340.0f, 1270.0f } } };//truck other side
+    gEnvBoundingBoxes[6] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 1259.0f, 327.0f, 1240.0f },(Vector3) { 1288.0f, 340.0f, 1244.0f } } };//truck back wall
 }
 
 #endif // DUCT_TAPE_H
