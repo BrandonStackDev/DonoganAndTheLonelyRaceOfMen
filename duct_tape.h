5280d5c6f72e5fa81d4befb52c37cc5b0e6e74a4
brandon stack <BrandonStack156@gmail.com>
2025-09-09 11:56:45 -0400

    book brick temple duct tape

--- duct_tape.h
+++ duct_tape.h
@@ -23,14 +23,14 @@ typedef struct
 
 
 
-#define gEnvBoundingBoxCount 13
+#define gEnvBoundingBoxCount 16
 
 EnvBoundingBox gEnvBoundingBoxes[gEnvBoundingBoxCount] = { 0 };
 
 void GoGoGadgetDuctTape()
 {
     gEnvBoundingBoxes[0] = (EnvBoundingBox){EBBT_WALL,(BoundingBox) {(Vector3) {2960.0f, 320.0f, 4026.0f},(Vector3) {2987.0f, 340.0f, 4027.0f}}};//don home side wall one
-    gEnvBoundingBoxes[1] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 2960.0f, 320.0f, 4058.0f },(Vector3) { 2987.0f, 340.0f, 4059.0f } } };//son home side wall
+    gEnvBoundingBoxes[1] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 2960.0f, 320.0f, 4058.0f },(Vector3) { 2987.0f, 340.0f, 4059.0f } } };//don home side wall
     gEnvBoundingBoxes[2] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 1281.0f, 327.0f, 1270.0f },(Vector3) { 1288.0f, 340.0f, 1272.0f } } };//truck short
     gEnvBoundingBoxes[3] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 1259.0f, 327.0f, 1270.0f },(Vector3) { 1266.0f, 340.0f, 1272.0f } } };//truck short
     gEnvBoundingBoxes[4] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 1257.8f, 327.0f, 1243.0f },(Vector3) { 1261.0f, 340.0f, 1270.0f } } };//truck side
@@ -42,6 +42,9 @@ void GoGoGadgetDuctTape()
     gEnvBoundingBoxes[10] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 1823.0f, 360, 4176.0f },(Vector3) { 1842.0f, 368.6f, 4183.0f } } };//nice 01 wall 2
     gEnvBoundingBoxes[11] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { -685, 560, 3723.0f },(Vector3) { -662.0f, 570, 3752.0f } } };//nice 02 wall 1
     gEnvBoundingBoxes[12] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { -605.0f, 558, 3690.0f },(Vector3) { -599.0f, 572, 3752.0f } } };//nice 02 wall 2
+    gEnvBoundingBoxes[13] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 2626, 333, 4490 },(Vector3) { 2749, 350, 4493 } } };//brick book temple wall
+    gEnvBoundingBoxes[14] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 2708, 320, 4614 },(Vector3) { 2751, 350, 4620.5f } } };//brick book temple wall small 01
+    gEnvBoundingBoxes[15] = (EnvBoundingBox){ EBBT_WALL,(BoundingBox) { (Vector3) { 2627, 320, 4614 },(Vector3) { 2670, 350, 4620.5f } } };//brick book temple wall small 02
 }
 
 #endif // DUCT_TAPE_H
