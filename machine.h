25eb98659a43a03da7ba8f4692260c05e07372e7
brandon stack <BrandonStack156@gmail.com>
2026-04-26 21:42:05 -0400

    rotate machines

--- machine.h
+++ machine.h
@@ -222,10 +222,10 @@ static inline void Machine_Init(void)
     //        don_pos = 2289.40, 338.82, 4761.52 //donogans
     //        don_pos = -717.63, 789.85, 2883.16 //yeti mt 1
     Machine_Add(MILL_MACHINE, 0, (Vector3) { 917.38, 326.02, 2004.90 }, 0.0f, 1.0f, SCENE_HOME_WINDMILL_10); //barn
-    Machine_Add(MILL_MACHINE, 1, (Vector3) { 2060.37, 461.00, 200.41 }, 0.0f, 1.0f, SCENE_HOME_WINDMILL_03);//far side castle
-    Machine_Add(MILL_MACHINE, 2, (Vector3) { 1941.82, 410.00, -526.68 }, 0.0f, 1.0f, SCENE_HOME_WINDMILL_04);
-    Machine_Add(MILL_MACHINE, 3, (Vector3) { 1821.41, 480.00, -1176.68 }, 0.0f, 1.0f, SCENE_HOME_WINDMILL_05);
-    Machine_Add(MILL_MACHINE, 4, (Vector3) { 1795.21, 509.00, -1717.80 }, 0.0f, 1.0f, SCENE_HOME_WINDMILL_06);
+    Machine_Add(MILL_MACHINE, 1, (Vector3) { 2060.37, 461.00, 200.41 }, PI / 2.0f, 1.0f, SCENE_HOME_WINDMILL_03);//far side castle
+    Machine_Add(MILL_MACHINE, 2, (Vector3) { 1941.82, 410.00, -526.68 }, PI / 2.0f, 1.0f, SCENE_HOME_WINDMILL_04);
+    Machine_Add(MILL_MACHINE, 3, (Vector3) { 1821.41, 480.00, -1176.68 }, PI / 2.0f, 1.0f, SCENE_HOME_WINDMILL_05);
+    Machine_Add(MILL_MACHINE, 4, (Vector3) { 1795.21, 509.00, -1717.80 }, PI / 2.0f, 1.0f, SCENE_HOME_WINDMILL_06);
     Machine_Add(MILL_MACHINE, 5, (Vector3) { 1938.34, 400.00, -2698.09 }, 0.0f, 1.0f, SCENE_HOME_WINDMILL_07);
     Machine_Add(MILL_MACHINE, 6, (Vector3) { -1700.79, 440.00, -1707.10 }, 0.0f, 1.0f, SCENE_HOME_WINDMILL_11);//past TOL
     Machine_Add(MILL_MACHINE, 7, (Vector3) { -2670.76, 410.00, -2397.02 }, 0.0f, 1.0f, SCENE_HOME_WINDMILL_08);//past past TOL
