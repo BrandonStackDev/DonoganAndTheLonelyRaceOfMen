94a757d2a2036f99b25120c5de74b9f47373f8c7
brandon stack <BrandonStack156@gmail.com>
2026-05-06 11:49:06 -0400

    start cave section

--- jc.h
+++ jc.h
@@ -375,6 +375,7 @@ static inline void InitHomeCollisionSets(void)
     LoadBuildingCollisionFile(MODEL_HOME_WINDMILL, "collide_homes/home_007.collide.txt");
     LoadBuildingCollisionFile(MODEL_HOME_NICE, "collide_homes/home_003.collide.txt");
     LoadBuildingCollisionFile(MODEL_CINDER, "collide_homes/cinder.collide.txt");
+    LoadBuildingCollisionFile(MODEL_CINDER_CAVE, "collide_homes/cave.collide.txt");
 }
 
 void InitHomes() {
